a7fd7eac604327fc22f9f5fe5fcab7c68b641da8
dvsource <virajkaush@gmail.com>
2022-01-05 00:41:10 +0530

    add some subject APIs

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const _ = require('lodash');
 
-const createSubject = (initialData) => {
-  let currentData = initialData;
-  const listners = new Map();
+const createSubject = (initialValue) => {
+  let _value = initialValue;
+  const _listners = new Map();
 
-  const _callListner = (data, listner) => {
+  let isComplted = false;
+  let hasError = false;
+
+  const _callListner = (value, listner) => {
     const { next, err, complete } = listner;
     try {
-      next(data);
+      next(value);
     } catch (e) {
       if (err) {
         err(e);
@@ -20,23 +23,59 @@ const createSubject = (initialData) => {
 
   const _createSubscription = (observer) => {
     const { next } = observer;
-    listners.set(next, observer);
-    if (initialData) {
-      _callListner(initialData, observer);
+    _listners.set(next, observer);
+    if (initialValue) {
+      _callListner(initialValue, observer);
     }
     return {
       unsubscribe: () => {
-        listners.delete(next);
+        _listners.delete(next);
       },
     };
   };
 
+  const _callErrorCallbacks = (err) => {
+    _listners.forEach((listner) => {
+      if (listner.err) {
+        listner.err(err);
+      }
+    });
+  };
+
+  const _callCompleteCallbacks = () => {
+    _listners.forEach((listner) => {
+      if (listner.complete) {
+        listner.complete();
+      }
+    });
+  };
+
   return {
-    next: (data) => {
-      currentData = data;
-      listners.forEach((listner) => {
-        _callListner(currentData, listner);
+    getValue: () => {
+      return _value;
+    },
+    next: (value) => {
+      if (isComplted) {
+        throw new Error('Subject is completed');
+      }
+      if (hasError) {
+        throw new Error('Subject has a error');
+      }
+      _value = value;
+      _listners.forEach((listner) => {
+        _callListner(_value, listner);
       });
+    },
+    error: (err) => {
+      _callErrorCallbacks(err);
+      hasError = true;
+    },
+    complete: () => {
+      _callCompleteCallbacks();
+      isComplted = true;
+    },
+    unsubscribe: () => {
+      _listners.clear();
     },
     asObservable: () => {
       return {
@@ -73,5 +112,6 @@ subject.next(6);
 subject.asObservable().subscribe((data) => {
   console.log(data);
 });
+// subject.complete();
 
-subject.next(10);
+// subject.next(10);
